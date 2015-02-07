43f7f7889919ef57471f1a82404a612ed3d33350
julian <julian.gallegos@gmail.com>
2015-02-06 17:17:39 -0800

    added images to rails file

--- app/assets/javascripts/matching.js
+++ app/assets/javascripts/matching.js
@@ -6,37 +6,66 @@ function Game(){
 	this.currentCards = []
 	this.remaining_cards = 20;
 	this.score = 0
+	this.images =[]
 }
 
-Game.prototype.images = [
-	"Rainbow Dash",
-	"Rainbow Dash",
-	"Pinkie Pie",
-	"Pinkie Pie",
-	"Twilight Sparker",
-	"Twilight Sparker",
+Game.prototype.imageClasses = [
+	"Rainbow_Dash",
+	"Rainbow_Dash",
+	"Pinkie_Pie",
+	"Pinkie_Pie",
+	"Twilight_Sparkle",
+	"Twilight_Sparkle",
 	"Fluttershy",
 	"Fluttershy",
 	"Rarity",
 	"Rarity",
-	"Apple Jack",	
-	"Apple Jack",
-	"Apple Bloom",
-	"Apple Bloom",
-	"Princess Celestia",
-	"Princess Celestia",
-	"Princess Luna",	
-	"Princess Luna",
+	"Apple_Jack",	
+	"Apple_Jack",
+	"Apple_Bloom",
+	"Apple_Bloom",
+	"Princess_Celestia",
+	"Princess_Celestia",
+	"Princess_Luna",	
+	"Princess_Luna",
 	"Spike",
 	"Spike",
 ]
 
-Game.prototype.addImagesToGame = function(){
-	for (var i = 0; i < this.images.length; i++){
-		this.cardHolder.push(this.images[i])
+Game.prototype.cardImages = [
+	"/assets/rainbow_dash.png",
+	"/assets/rainbow_dash.png",
+	"/assets/pinkie_pie.png",
+	"/assets/pinkie_pie.png",
+	"/assets/twilight_sparkle.png",
+	"/assets/twilight_sparkle.png",
+	"/assets/fluttershy.png",
+	"/assets/fluttershy.png",
+	"/assets/rarity.png",
+	"/assets/rarity.png",
+	"/assets/apple_jack.png",
+	"/assets/apple_jack.png",
+	"/assets/apple_bloom.png",
+	"/assets/apple_bloom.png",
+	"/assets/princess_celestia.png",
+	"/assets/princess_celestia.png",
+	"/assets/princess_luna.png",
+	"/assets/princess_luna.png",
+	"/assets/spike.png",
+	"/assets/spike.png",
+]
+
+Game.prototype.addImagesClassesToGame = function(){
+	for (var i = 0; i < this.imageClasses.length; i++){
+		this.cardHolder.push(this.imageClasses[i])
 	}
 }
 
+Game.prototype.addImagesToGame = function(){
+	for(var i = 0; i < this.cardImages.length; i++){
+		this.images.push(this.cardImages[i])
+	}
+}
 
 Game.prototype.comparedSelectedCards = function(){
 	if (this.currentCards[0] === this.currentCards[1]){
@@ -102,12 +131,20 @@ View.prototype.resetViewScore = function(){
 	$('#score').text(0)
 }
 
-View.prototype.addCardImagesToView = function(images){
-	$('.hidden_card').each(function renderImagesToView(index, element){
+View.prototype.addCardClassToView = function(images){
+	$('.hidden_card').each(function renderClassesToView(index, element){
 		$(element).attr("id", images[index])
-		$(element).append(images[index])
+		// $(element).append(images[index])
 	})
 }
+
+View.prototype.addCardImagesToView = function(images){
+	console.log(images)
+	$('.hidden_card').each(function renderImagesToView(index, element){
+		$(element).append('<img src=' +images[index] +'>')
+	})
+}
+
 
 //===============================Controller===========================
 
@@ -117,8 +154,10 @@ function Controller(view, model){
 }
 
 Controller.prototype.startGame = function(images){
+	this.model.addImagesClassesToGame();
 	this.model.addImagesToGame();
-	this.view.addCardImagesToView(this.model.cardHolder);
+	this.view.addCardClassToView(this.model.cardHolder);
+	this.view.addCardImagesToView(this.model.images)
 }
 
 Controller.prototype.turnOverCard = function(){
